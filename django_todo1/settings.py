e2c12047bcd0d90fedc4dc50f860af43f08dcbf1
samcat437 <sdrees12@gmail.com>
2022-08-16 20:53:16 +0000

    created superuser and todo migrations and templates

--- django_todo1/settings.py
+++ django_todo1/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-47cxcdrwl*nl!3w0b_sq&hs@p$n!eql($4^(l)lb3(u8ty6#@r'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
