8877908fe623ffdf4f059ffb136d6b00fa1755e5
samcat437 <sdrees12@gmail.com>
2022-09-26 11:18:16 +0000

    removed secret key, still not building to heroku

--- django_todo1/settings.py
+++ django_todo1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-47cxcdrwl*nl!3w0b_sq&hs@p$n!eql($4^(l)lb3(u8ty6#@r')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -34,7 +34,7 @@ if development:
     ALLOWED_HOSTS = [os.environ.get('localhost')]
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-    
+
 
 # Application definition
 
